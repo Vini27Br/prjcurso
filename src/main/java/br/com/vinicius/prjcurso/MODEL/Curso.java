9778dc5ab1e6a945a021132384613257272e2641
Vini27Br <viniciusouza0@gmail.com>
2022-09-12 08:51:25 -0300

    get and set adicionado

--- src/main/java/br/com/vinicius/prjcurso/MODEL/Curso.java
+++ src/main/java/br/com/vinicius/prjcurso/MODEL/Curso.java
@@ -1,6 +1,15 @@
 package br.com.vinicius.prjcurso.MODEL;
 
 public class Curso {
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
     private Integer id;
     private String  namecurso;
 }
+
