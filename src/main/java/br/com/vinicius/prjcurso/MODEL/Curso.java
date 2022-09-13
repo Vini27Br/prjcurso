0357d7a877dc6db54a82ab184358b6afe4d0731e
Vini27Br <viniciusouza0@gmail.com>
2022-09-13 09:32:49 -0300

    equals e hashcode

--- src/main/java/br/com/vinicius/prjcurso/MODEL/Curso.java
+++ src/main/java/br/com/vinicius/prjcurso/MODEL/Curso.java
@@ -1,15 +1,42 @@
 package br.com.vinicius.prjcurso.MODEL;
 
+import java.util.Objects;
+
 public class Curso {
+    private Integer id;
+    private String  namecurso;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Curso curso = (Curso) o;
+        return id.equals(curso.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
     public Integer getId() {
         return id;
+    }
+
+
+
+    public String getNamecurso() {
+        return namecurso;
+    }
+
+
+    public void setNamecurso(String namecurso) {
+        this.namecurso = namecurso;
     }
 
     public void setId(Integer id) {
         this.id = id;
     }
 
-    private Integer id;
-    private String  namecurso;
 }
 
