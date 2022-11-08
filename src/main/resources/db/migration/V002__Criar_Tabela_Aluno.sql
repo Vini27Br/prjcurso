5c41d9640a43e68b4eab11948f0cc962727227a4
Vini27Br <viniciusouza0@gmail.com>
2022-11-08 09:36:51 -0300

    Tabela Aluno

--- src/main/resources/db/migration/V002__Criar_Tabela_Aluno.sql
+++ src/main/resources/db/migration/V002__Criar_Tabela_Aluno.sql
@@ -7,4 +7,8 @@ anodeconclusao int not null
 
 Alter Table Aluno add CONSTRAINT FK_Aluno_Curso foreign key(cursoid) references curso(id);
 
-insert into Aluno(nomealuno, cursoid, anodeconclusao)
+insert into Aluno(nomealuno, cursoid, anodeconclusao) values
+("Huginho",1,2010),
+("Zezinho",2,2011),
+("Luizinho",3,2012),
+("Ricardão",3,2007);
