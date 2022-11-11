2c4ae5d3eb1ee01a39fa67e475f8338ed1df7ac0
Vini27Br <viniciusouza0@gmail.com>
2022-11-11 08:47:50 -0300

    Finalizado com erros

--- src/main/java/br/com/vinicius/prjcurso/resource/CursoResource.java
+++ src/main/java/br/com/vinicius/prjcurso/resource/CursoResource.java
@@ -2,7 +2,9 @@ package br.com.vinicius.prjcurso.resource;
 
 import br.com.vinicius.prjcurso.MODEL.Curso;
 import br.com.vinicius.prjcurso.repository.CursoRepository;
+import javafx.scene.effect.Light;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,6 @@ public class CursoResource {
 
     @GetMapping("/todos")
     public List<Curso> listarTodosCursos(){
-        return cursoRepository.findAll();
+        return cursoRepository.findAll(Sort.by("nomecurso"));
     }
 }
